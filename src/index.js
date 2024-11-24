e5e1345222d6fda47e6e9568c6a5d1eb0f9be21c
jonah <jonahkf@gmail.com>
2024-11-24 19:07:25 +0900

    ADDED: Ability to pull London's weather data & store in variable

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
 import "./styles.css";
+
+const screenController = () => {
+  const key = "MPY6ZZCCS2KYRC8YBYF7H9RXP"; // Pls don't :')
+  let city = "london";
+
+  async function getWeather(city, key) {
+    try {
+      const response = await fetch(
+        `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=${key}`,
+        { mode: "cors" },
+      );
+      const weatherData = await response.json();
+      console.log("Fetched the following:", weatherData);
+      return weatherData;
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  let currentCityWeather = getWeather(city, key);
+
+  const updateDisplay = (currentCityWeather) => {};
+};
+
+screenController();
