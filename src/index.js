26386060c9f6f8af5ee166d67ba173a27fa9a777
jonah <jonahkf@gmail.com>
2024-11-26 19:44:14 +0900

    ADDED: Layout, Time of Day Gradients, Basic City Functionality

--- src/index.js
+++ src/index.js
@@ -2,7 +2,15 @@ import "./styles.css";
 
 const screenController = () => {
   const key = "MPY6ZZCCS2KYRC8YBYF7H9RXP"; // Pls don't :')
-  let city = "london";
+  let celsiusFlag = true;
+
+  const weatherDisplay = document.querySelector(".weather");
+  const tempDisplay = document.querySelector(".temp");
+  const weatherIconDisplay = document.querySelector(".weather-icon");
+  const cityDisplay = document.querySelector(".city-name");
+
+  // Default city
+  let city = "kamakura";
 
   async function getWeather(city, key) {
     try {
@@ -12,15 +20,44 @@ const screenController = () => {
       );
       const weatherData = await response.json();
       console.log("Fetched the following:", weatherData);
-      return weatherData;
+      updateDisplay(weatherData, city);
     } catch (err) {
       throw err;
     }
   }
 
-  let currentCityWeather = getWeather(city, key);
+  getWeather(city, key);
 
-  const updateDisplay = (currentCityWeather) => {};
+  const updateDisplay = (weather, city) => {
+    let conditions = weather.currentConditions.conditions;
+    let temp = weather.currentConditions.temp;
+    celsiusFlag === true ? (temp = (temp - 32) / (9 / 5)) : (temp = temp);
+    let cityName = weather.resolvedAddress;
+
+    weatherDisplay.textContent = `${conditions}`;
+    tempDisplay.textContent = `${Math.round(temp)}°`;
+    // weatherIconDisplay
+    cityDisplay.textContent = cityName;
+  };
+
+  const updateGradient = (time) => {
+    // Convert time to HH format (0 to 23)
+    let convertedTime = time;
+    const leftChildTop = document.querySelector(".left-child-top");
+    leftChildTop.background = `var(--gradient-${convertedTime})`;
+  };
+
+  const updateForecasts = (currentCityWeather) => {};
+
+  const setSunriseSunset = () => {};
+
+  const searchCities = () => {};
+
+  const displaySettings = () => {};
+
+  const toggleTempAndWeatherColor = () => {};
+
+  const toggleCityNameColor = () => {};
 };
 
 screenController();
