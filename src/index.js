e81af6c5471b9194d8f87cddf757cc75fdfb6d2f
jonah <jonahkf@gmail.com>
2024-11-30 20:05:28 +0900

    ADDED: Search functionality, display graphics

--- src/index.js
+++ src/index.js
@@ -13,17 +13,13 @@ const screenController = () => {
   let city = "kamakura";
 
   async function getWeather(city, key) {
-    try {
-      const response = await fetch(
-        `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=${key}`,
-        { mode: "cors" },
-      );
-      const weatherData = await response.json();
-      console.log("Fetched the following:", weatherData);
-      updateDisplay(weatherData, city);
-    } catch (err) {
-      throw err;
-    }
+    const response = await fetch(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=${key}`,
+      { mode: "cors" },
+    );
+    const weatherData = await response.json();
+    console.log("Fetched the following:", weatherData);
+    updateDisplay(weatherData, city);
   }
 
   getWeather(city, key);
@@ -31,33 +27,49 @@ const screenController = () => {
   const updateDisplay = (weather, city) => {
     let conditions = weather.currentConditions.conditions;
     let temp = weather.currentConditions.temp;
-    celsiusFlag === true ? (temp = (temp - 32) / (9 / 5)) : (temp = temp);
-    let cityName = weather.resolvedAddress;
+    celsiusFlag === true ? (temp = (temp - 32) / (9 / 5)) : temp;
+    let cityName = weather.resolvedAddress
+      .split(",")[0]
+      .split("-")[0]
+      .toUpperCase();
 
     weatherDisplay.textContent = `${conditions}`;
     tempDisplay.textContent = `${Math.round(temp)}°`;
     // weatherIconDisplay
     cityDisplay.textContent = cityName;
+
+    let cityField = document.querySelector("#city");
+    cityField.placeholder = weather.resolvedAddress.split(",")[0].split("-")[0];
+
+    let localTime = weather.currentConditions.datetime;
+    updateGradient(localTime);
   };
 
   const updateGradient = (time) => {
-    // Convert time to HH format (0 to 23)
-    let convertedTime = time;
-    const leftChildTop = document.querySelector(".left-child-top");
-    leftChildTop.background = `var(--gradient-${convertedTime})`;
+    console.log(time);
+    let convertedTime = time.split(":")[0];
+    const childTop = document.querySelector(".child-top");
+    const childBottom = document.querySelector(".child-bottom");
+    childTop.style.background = `var(--gradient-${convertedTime})`;
+    childBottom.style.color = `var(--color-${convertedTime})`;
   };
 
   const updateForecasts = (currentCityWeather) => {};
 
   const setSunriseSunset = () => {};
 
-  const searchCities = () => {};
+  const form = document.querySelector("#search-form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let cityField = document.querySelector("#city");
+    let searchTerm = cityField.value;
+    cityField.value = "";
+    getWeather(searchTerm, key);
+  });
+
+  // const searchCities = (city) => {};
 
   const displaySettings = () => {};
-
-  const toggleTempAndWeatherColor = () => {};
-
-  const toggleCityNameColor = () => {};
 };
 
 screenController();
